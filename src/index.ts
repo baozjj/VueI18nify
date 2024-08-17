e93a38efce0d75eda6500436edf58a9e3fb190af
baozhangjie <baozhangjie@xiaohongshu.com>
2024-08-17 23:38:23 +0800

    feat：add main，测试js文件

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log(12312312);
+import { readFile } from "node:fs/promises";
+import { parseJS } from "./parsers/jsParser";
+import { JsParserType } from "./service/interface";
+
+const main = async () => {
+  const jsPath = "/Users/baozhangjie/Desktop/VueI18nify/src/test/js-test.js";
+
+  const res = await readFile(jsPath, "utf-8");
+  const jsAst = parseJS(res, JsParserType.js);
+  console.log("jsAstjsAst", jsAst);
+};
+
+main();
