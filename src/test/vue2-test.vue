16f20fd15323989cf2ce8000556760e2aeec2cfd
baozhangjie <baozhangjie@xiaohongshu.com>
2024-08-18 13:10:54 +0800

    feat: add vue2 测试文件

---
+++ src/test/vue2-test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ 你好啊 }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "你好",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
