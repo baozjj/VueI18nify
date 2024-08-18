0b156050274a040e97ef359f5d75f06bc9a7590b
baozhangjie <baozhangjie@xiaohongshu.com>
2024-08-18 10:07:29 +0800

    feat:文件的扩展名

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import { parseJS } from "./parsers/jsParser";
 import { JsParserType } from "./service/interface";
 import { transformJS } from "./transformers/transformJS";
 import { generateJS } from "./generates/generatesJS";
+import { extname } from "node:path";
 
 const main = async () => {
   const jsPath = "/Users/baozhangjie/Desktop/VueI18nify/src/test/js-test.js";
 
-  const res = await readFile(jsPath, "utf-8");
-  const jsAst = parseJS(res, JsParserType.js);
+  // 使用 readFile 方法读取文件内容
+  const content = await readFile(jsPath, "utf-8");
+
+  // 判断文件的扩展名
+  const fileExtname = extname(jsPath);
+
+  const jsAst = parseJS(content, JsParserType.js);
   transformJS(jsAst);
 
   const codeRes = generateJS(jsAst);
   console.log("jsAstjsAst", codeRes);
+  console.log("fileExtname", fileExtname);
 };
 
 main();
