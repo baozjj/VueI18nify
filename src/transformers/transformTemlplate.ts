ddb7e698ba87c5e0811ed5041f497b47301d06af
baozhangjie <baozhangjie@xiaohongshu.com>
2024-08-27 00:46:10 +0800

    feat: 处理插值表达式 SIMPLE_EXPRESSION类型

--- src/transformers/transformTemlplate.ts
+++ src/transformers/transformTemlplate.ts
@@ -1,17 +1,31 @@
-import { NodeTypes, ElementNode, TextNode } from "@vue/compiler-dom";
+import {
+  NodeTypes,
+  ElementNode,
+  TextNode,
+  InterpolationNode,
+  SimpleExpressionNode,
+} from "@vue/compiler-dom";
 import type { TemplateChildNode } from "@vue/compiler-dom";
+import { handleJs } from "../handlers";
+import { containsChinese } from "../utils/regex";
 
 export const transformTemplate = (astTree: TemplateChildNode[]): string => {
   let result = "";
 
   for (const node of astTree) {
     let endTag = true;
+    console.log(node.type);
+
     switch (node.type) {
       case NodeTypes.ELEMENT:
         result += transformElement(node as ElementNode);
         break;
       case NodeTypes.TEXT:
         result += transformText(node as TextNode);
+        endTag = false;
+        break;
+      case NodeTypes.INTERPOLATION:
+        result += transformInterpolation(node as InterpolationNode);
         endTag = false;
         break;
       default:
@@ -30,7 +44,17 @@ const transformElement = (node: ElementNode): string => {
   return res;
 };
 
-function transformText(node: TextNode): string {
-  let res = `{{ $t(${node.content}) }}`;
+const transformText = (node: TextNode): string => {
+  if (!containsChinese(node.content)) return node.content;
+  let res = `{{ $t('${node.content}') }}`;
   return res;
-}
+};
+const transformInterpolation = (node: InterpolationNode): string => {
+  let res = "";
+  if (node.content.type === NodeTypes.SIMPLE_EXPRESSION) {
+    res = handleJs(node.content.content);
+  }
+  res = `{{ ${res} }}`;
+
+  return res;
+};
