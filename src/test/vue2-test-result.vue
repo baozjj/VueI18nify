b9637b0c53a5a3989656592c7cd882db003e184d
baozhangjie <baozhangjie@xiaohongshu.com>
2024-08-27 22:55:06 +0800

    feat: props 事件处理

--- src/test/vue2-test-result.vue
+++ src/test/vue2-test-result.vue
@@ -1,15 +1,17 @@
-<template><div>{{ 1 + "123" }}123 {{ true ? $t('阿斯顿') : $t('阿斯') }}
- </div><div :aaa="func($t('啊啊啊'))" :a="$t('你好')" b="123">{{ $t('你好啊2') }}
- </div>
- </template> 
- <script> 
- export default {
+<template>
+  <div>{{ 1 + "123" }}123 {{ true ? $t("阿斯顿") : $t("阿斯") }}</div>
+  <div :aaa="func($t('啊啊啊'))" :a="$t('你好')" @click="func($t('啊啊1啊'))">
+    {{ $t("你好啊2") }}
+  </div>
+</template>
+<script>
+export default {
   data() {
     return {
-      test: $t('你好')
+      test: $t("你好"),
     };
   },
-  methods: {}
-} 
-</script> 
- <style scoped lang="less"></style>
+  methods: {},
+};
+</script>
+<style scoped lang="less"></style>
